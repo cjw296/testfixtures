ad76ecdc4c360aeb9ace612ac5dc1fd30fb3a137
Chris Withers <chris@withers.org>
2025-03-30 11:44:07 +0100

    Comment everything out

--- testfixtures/__init__.py
+++ testfixtures/__init__.py
@@ -16,7 +16,7 @@ from testfixtures.comparison import (
     Comparison, StringComparison, RoundComparison, compare, diff, RangeComparison,
     SequenceComparison, Subset, Permutation, MappingComparison
 )
-from testfixtures.datetime import mock_datetime, mock_date, mock_time
+# from testfixtures.datetime import mock_datetime, mock_date, mock_time
 from testfixtures.logcapture import LogCapture, log_capture
 from testfixtures.outputcapture import OutputCapture
 from testfixtures.resolve import resolve
@@ -35,12 +35,12 @@ from testfixtures.utils import wrap, generator
 
 
 # backwards compatibility for the old names
-test_datetime = mock_datetime
-test_datetime.__test__ = False  # type: ignore[attr-defined]
-test_date = mock_date
-test_date.__test__ = False  # type: ignore[attr-defined]
-test_time = mock_time
-test_time.__test__ = False  # type: ignore[attr-defined]
+# test_datetime = mock_datetime
+# test_datetime.__test__ = False  # type: ignore[attr-defined]
+# test_date = mock_date
+# test_date.__test__ = False  # type: ignore[attr-defined]
+# test_time = mock_time
+# test_time.__test__ = False  # type: ignore[attr-defined]
 
 __all__ = [
     'Comparison',
@@ -64,9 +64,9 @@ __all__ = [
     'diff',
     'generator',
     'log_capture',
-    'mock_date',
-    'mock_datetime',
-    'mock_time',
+    # 'mock_date',
+    # 'mock_datetime',
+    # 'mock_time',
     'not_there',
     'replace',
     'replace_in_environ',
@@ -76,8 +76,8 @@ __all__ = [
     'should_raise',
     'singleton',
     'tempdir',
-    'test_date',
-    'test_datetime',
-    'test_time',
+    # 'test_date',
+    # 'test_datetime',
+    # 'test_time',
     'wrap',
 ]
